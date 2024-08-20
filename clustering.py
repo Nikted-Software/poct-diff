e3a48c118ba9b5ffcd678eff9baea2000f9cc345
Mostafa Ghazanfari <M.Ghazanfari@nikted.local>
2024-08-20 15:08:45 +0330

    code optimized and cluster algorithm change into function

--- clustering.py
+++ clustering.py
@@ -3,59 +3,81 @@ from sklearn.cluster import SpectralClustering
 import matplotlib.pyplot as plt
 from pandas import DataFrame
 import pandas as pd
+from sklearn.cluster import KMeans
+import os
+from natsort import natsorted
+import cv2
 
-#params
-n_clusters = 2
-dataset = pd.read_csv("feature.csv")
+def kmeans_clustering(df, path_i, n_clusters):
+    
+    kmeans = KMeans(n_clusters=n_clusters, init="random", max_iter=300, n_init=100)
+    y_kmeans = kmeans.fit_predict(df.iloc[:, [1, 2]])
+    cluster_counts = [0] * n_clusters
+    for label in y_kmeans:
+        cluster_counts[label] += 1
+    included_extensions = ["jpg", "jpeg", "bmp", "png", "gif", "JPG"]
+    file_names = [
+        fn
+        for fn in os.listdir(path_i)
+        if any(fn.endswith(ext) for ext in included_extensions)
+    ]
 
-#preprocessing
-dataset = dataset.drop(dataset.columns[0], axis=1)
-data = dataset.iloc[:, :].values
-x = dataset.iloc[:, [1,2]].values
-x1 = dataset.iloc[:, [1,2]].values
-x1[:,[0,1]] = x1[:,[0,1]]/255
+    natsort_file_names = natsorted(file_names)
+    for m, file_name in enumerate(natsort_file_names):
+        im = cv2.imread(os.path.join(path_i, file_name))
+        cluster_label = y_kmeans[m]
+        cluster_folder = f"a2/{str(cluster_label+1).zfill(2)}"
+        if not os.path.exists(cluster_folder):
+            os.makedirs(cluster_folder)
+        cv2.imwrite(f"{cluster_folder}/{m+1}_class{cluster_label}.jpg", im)
 
-#plot data
-fig1=plt.figure()
-plt.scatter(data[:, [2]], data[:, [1]])
-plt.savefig("data.png")
-plt.close(fig1)
+    X = df.iloc[:, [2, 1]].values
+    colors = ["blue", "red", "green", "yellow", "purple", "orange", "cyan", "magenta", "brown", "pink"]
+    for i in range(n_clusters):
+        plt.scatter(X[y_kmeans == i, 0], X[y_kmeans == i, 1], s=10, c=colors[i % len(colors)], label=f"Cluster {i+1}")
+    plt.scatter(kmeans.cluster_centers_[:, 1], kmeans.cluster_centers_[:, 0], s=100, c="black", label="Centroids")
+    plt.title(f"KMeans Clustering with {n_clusters} Clusters")
+    plt.xlabel("r")
+    plt.ylabel("g")
+    plt.legend()
+    plt.show()
 
+def gaussian_mixture_clustering(data, n_clusters, output_path="gm.png"):
+    
+    gm = GaussianMixture(
+        n_components=n_clusters,
+        covariance_type="full",
+        n_init=100,
+        init_params="kmeans",
+        max_iter=100,
+    )
+    pred = gm.fit_predict(data)
+    df = DataFrame({"x": data[:, 2], "y": data[:, 1], "label": pred})
+    groups = df.groupby("label")
+    fig, ax = plt.subplots()
+    for name, group in groups:
+        ax.scatter(group.x, group.y, label=name)
+    ax.legend()
 
-gm = GaussianMixture(
-    n_components=n_clusters,
-    covariance_type="full",
-    n_init=100,
-    init_params="kmeans",
-    max_iter=100,
-)
+    plt.savefig(output_path)
+    plt.close(fig)
 
-pred = gm.fit_predict(x)
-df = DataFrame({"x": data[:, 2],  "y": data[:, 1], "label": pred})
-groups = df.groupby("label")
-fig2, ax = plt.subplots()
-for name, group in groups:
-    ax.scatter(group.x, group.y, label=name)
-ax.legend()
-plt.savefig("gm.png")
-plt.close(fig2)
-
-clustering = SpectralClustering(
+def spectral_clustering(data, x1, n_clusters, output_path="sp.png"):
+    
+    clustering = SpectralClustering(
         n_clusters=n_clusters,
         assign_labels='discretize',
-        random_state=0)
-pred =  clustering.fit_predict(x1)
-df = DataFrame({"x": data[:, 2],  "y": data[:, 1], "label": pred})
-groups = df.groupby("label")
-fig3, ax = plt.subplots()
-for name, group in groups:
-    ax.scatter(group.x, group.y, label=name)
-ax.legend()
-plt.savefig("sp.png")
-plt.close(fig3)
-
-dataset[5] = pred
-
-#print(dataset)
-print(sum(pred),len(pred))
-dataset.to_csv('output_with_clusters.csv', index=False)
+        random_state=0
+    )
+    pred = clustering.fit_predict(x1)
+    df = DataFrame({"x": data[:, 2], "y": data[:, 1], "label": pred})
+    groups = df.groupby("label")
+    fig, ax = plt.subplots()
+    for name, group in groups:
+        ax.scatter(group.x, group.y, label=name)
+    ax.legend()
+    plt.savefig(output_path)
+    plt.close(fig)
+    data = DataFrame(data)
+    data[5] = pred
+    
--- main.py
+++ main.py
@@ -7,10 +7,9 @@ import os
 import glob
 from natsort import natsorted
 from threshold_sauvola import sau
-from sklearn.cluster import KMeans
 import math
 from feature_extraction import feature_extraction
-
+from clustering import kmeans_clustering,spectral_clustering,gaussian_mixture_clustering
 
 import os
 import glob
@@ -56,7 +55,7 @@ clear_directory(paths_to_clear)
 create_directories(paths_to_create)
 
 image_folder = "data"
-image_name = "7751_crop.jpeg"
+image_name = "7791_crop.jpeg"
 image_name = f"{image_folder}/{image_name}"
 
 image1 = cv2.imread(image_name)
@@ -93,38 +92,23 @@ for i in natsort_file_names:
        cv2.imwrite(f"a2/1/{m+1}.jpg", im)
     m += 1
 
-def kmeans_clustering(df, path_i, n_clusters):
-    
-    kmeans = KMeans(n_clusters=n_clusters, init="random", max_iter=300, n_init=100)
-    y_kmeans = kmeans.fit_predict(df.iloc[:, [1, 2]])
-    cluster_counts = [0] * n_clusters
-    for label in y_kmeans:
-        cluster_counts[label] += 1
-    included_extensions = ["jpg", "jpeg", "bmp", "png", "gif", "JPG"]
-    file_names = [
-        fn
-        for fn in os.listdir(path_i)
-        if any(fn.endswith(ext) for ext in included_extensions)
-    ]
+#params
+n_clusters = 2
+dataset = pd.read_csv("feature.csv")
 
-    natsort_file_names = natsorted(file_names)
-    for m, file_name in enumerate(natsort_file_names):
-        im = cv2.imread(os.path.join(path_i, file_name))
-        cluster_label = y_kmeans[m]
-        cluster_folder = f"a2/{str(cluster_label+1).zfill(2)}"
-        if not os.path.exists(cluster_folder):
-            os.makedirs(cluster_folder)
-        cv2.imwrite(f"{cluster_folder}/{m+1}_class{cluster_label}.jpg", im)
+#preprocessing
+dataset = dataset.drop(dataset.columns[0], axis=1)
+data = dataset.iloc[:, :].values
+x = dataset.iloc[:, [1,2]].values
+x1 = dataset.iloc[:, [1,2]].values
+x1[:,[0,1]] = x1[:,[0,1]]/255
 
-    X = df.iloc[:, [2, 1]].values
-    colors = ["blue", "red", "green", "yellow", "purple", "orange", "cyan", "magenta", "brown", "pink"]
-    for i in range(n_clusters):
-        plt.scatter(X[y_kmeans == i, 0], X[y_kmeans == i, 1], s=10, c=colors[i % len(colors)], label=f"Cluster {i+1}")
-    plt.scatter(kmeans.cluster_centers_[:, 1], kmeans.cluster_centers_[:, 0], s=100, c="black", label="Centroids")
-    plt.title(f"KMeans Clustering with {n_clusters} Clusters")
-    plt.xlabel("r")
-    plt.ylabel("g")
-    plt.legend()
-    plt.show()
+#plot data
+fig1=plt.figure()
+plt.scatter(data[:, [2]], data[:, [1]])
+plt.savefig("data.png")
+plt.close(fig1)
 
 kmeans_clustering(df, "a", n_clusters=2)
+gaussian_mixture_clustering(data, n_clusters=n_clusters, output_path="gm.png")
+updated_data = spectral_clustering(data, x1, n_clusters=n_clusters, output_path="sp.png")
