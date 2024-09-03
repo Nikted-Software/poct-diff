9c7768643b2249942a7875e2211941336bb3bbdd
Mostafa Ghazanfari <M.Ghazanfari@nikted.local>
2024-09-03 10:40:35 +0330

    clustering algorithm perform on lymphocyte_monocyte to seperate them

--- lymphocyte_monocyte_diff.py
+++ lymphocyte_monocyte_diff.py
@@ -1,24 +1,21 @@
 import os
 import cv2
-from otsu import otsu
+import pandas as pd
+import numpy as np
+from clustering import kmeans_clustering, spectral_clustering, gaussian_mixture_clustering
+import matplotlib.pyplot as plt
 
-folder_path = 'a/gaussian_mixture/01'
-image_files = [f for f in os.listdir(folder_path) if f.endswith(('.png', '.jpg', '.jpeg', '.bmp', '.gif', '.tiff'))]
-i = 0
-are = []
-thf0 = []
-thf = []
-while i < len(image_files):
-    file_path = os.path.join(folder_path, image_files[i])
-    img = cv2.imread(file_path)
-    i += 1
-    th = otsu(img)
-    image00 = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    ret, thresh0 = cv2.threshold(image00, th, 255, cv2.THRESH_BINARY)
-    are.append(cv2.contourArea(c))
-    for q in range(0, thresh0.shape[0], 1):
-        for g in range(0, thresh0.shape[1], 1):
-            if thresh0[q, g] == 255:
-                thf0.append(image0[q, g, :])
-    ee = np.array(thf0)
-    thf.append(np.mean(ee, axis=0))
+
+dataset = pd.read_csv("a/gaussian_mixture/file_labels.csv")
+filtered_dataset = dataset[dataset['cluster_label'] == 1]
+x_final = filtered_dataset.iloc[:, [3, 4]].values
+
+x_final[:, 0] = x_final[:, 0] / np.max(x_final[:, 0])
+x_final[:, 1] = x_final[:, 1] / np.max(x_final[:, 1])
+
+fig1=plt.figure()
+plt.scatter(x_final[:, [0]], x_final[:, [1]])
+plt.savefig("lym_mon.png")
+plt.close(fig1)
+
+gaussian_mixture_clustering(filtered_dataset, x_final, x_final, "a/gaussian_mixture/02", n_clusters=2)
